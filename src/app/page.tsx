873d7add42a79ccea59908806553d0102c801cac
tch <tristan.chretien@tenacy.io>
2024-05-01 11:19:24 +0200

    feat: setup database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/b7d70031-7688-414c-9260-65b4dcc444e2-g91b3o.jpg",
@@ -12,13 +13,23 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+         {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
+            </div>
+         ))}
         {
-          [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
-            <div key={image.id} className="w-48">
+          [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+            <div key={image.id + "-" + index} className="w-48">
               <img src={image.url} alt="" className="w-full h-48 object-cover" />
             </div>
           ))
