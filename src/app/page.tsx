bbae4853dbeb3912783d21f51f46bcdde36c1f83
tch <tristan.chretien@tenacy.io>
2024-05-01 10:54:19 +0200

    feat: starting ui mockImages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/b7d70031-7688-414c-9260-65b4dcc444e2-g91b3o.jpg",
+  "https://utfs.io/f/0b85cfa5-1327-4f7b-bef5-864757aab3e0-n3cql3.jpg",
+  "https://utfs.io/f/930386b7-bda4-4a93-a63f-2d43031224fd-gon6qa.webp",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}));
+
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        Hello project in progress
-      </div>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {
+          [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} alt="" className="w-full h-48 object-cover" />
+            </div>
+          ))
+        }
+        </div>
     </main>
   );
 }
